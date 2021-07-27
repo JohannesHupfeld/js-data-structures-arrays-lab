58e490e4b130874e1ab9b28ddc69d834b17ec1af
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-27 14:59:21 -0400

    commit tree 3d5c69eb9d6d2389360f3ef834a34d2bc703ffb5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(Ralph){
+  drivers.push(Ralph);
+}
+
+function destructivelyPrependDriver(Bob){
+  drivers.unshift(Bob);
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let newDrivers = [...drivers, name];
+ return newDrivers;
+}
+
+function prependDriver(name) {
+  let newDrivers = [name,...drivers];
+  return newDrivers;
+ }
+ 
+ function removeLastDriver() {
+   let newDrivers = drivers.slice(0, drivers.length - 1);
+   return newDrivers;
+ }
+ 
+ function removeFirstDriver() {
+   let newDrivers = drivers.slice(1);
+   return newDrivers;
+ }
